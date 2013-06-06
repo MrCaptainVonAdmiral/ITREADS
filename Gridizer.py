faf559ba12ee1f0f6a320a07fe35371c4e19b4a9
c0z3n <gordon.p.clement@gmail.com>
2013-06-06 19:58:29 -0400

    fix list malarkey

--- Gridizer.py
+++ Gridizer.py
@@ -8,12 +8,12 @@ Col = x2 - x1
 TL=[x1,y1]
 BR=[x2,y2]
 print TL, BR
-clr=[[]]
+clr=list(list()) # this sets up a list of lists, so a multidimensional list accessed as[x][y]
 image = ImageGrab.grab()
 for i in xrange(Row):
     Row_Read=i+x1
     for j in xrange(Col):
         Col_Read=y1+j
-        clr[Row_Read,Col_Read] = image.getpixel((Row_Read, Col_Read))
-        print clr[Row_Read,Col_Read]
+        clr[Row_Read][Col_Read] = image.getpixel((Row_Read, Col_Read))
+        print clr[Row_Read][Col_Read]
     
