63d5994190b68522713a42d5748b7f50dc8f4fdc
HMAN <Hws8137@rit.edu>
2013-06-06 19:53:12 -0400

    MY LIST INDICES, THEY ARE NOT BEING MY INTEGERS!
    ARGHH!! PEANUTS!

--- Gridizer.py
+++ Gridizer.py
@@ -2,16 +2,18 @@ import ImageGrab
 
 #Bound box coords
 x1, x2, y1, y2 = 622, 636, 44, 59
-Row = y1 - y2
+Row = y2 - y1
 Col = x2 - x1
 
 TL=[x1,y1]
 BR=[x2,y2]
 print TL, BR
-
+clr=[[]]
 image = ImageGrab.grab()
-for i in xrange(Row)
-    for j in xrange(Col)
-    color(i,j) = image.getpixel((i, j))
-    print color(i,j)
+for i in xrange(Row):
+    Row_Read=i+x1
+    for j in xrange(Col):
+        Col_Read=y1+j
+        clr[Row_Read,Col_Read] = image.getpixel((Row_Read, Col_Read))
+        print clr[Row_Read,Col_Read]
     
