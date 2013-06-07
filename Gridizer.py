36ce068325b2223d89db56637d2355fbe20fc344
HMAN <Hws8137@rit.edu>
2013-06-06 20:18:22 -0400

    IT WORKS WARPS!

--- Gridizer.py
+++ Gridizer.py
@@ -8,12 +8,19 @@ Col = x2 - x1
 TL=[x1,y1]
 BR=[x2,y2]
 print TL, BR
-clr=list(list()) # this sets up a list of lists, so a multidimensional list accessed as[x][y]
+color=list(list())
+
+for k in xrange(16):
+    temp_list=list()
+    for q in xrange(16):
+        temp_list.append(0)
+    color.append(temp_list)
+
 image = ImageGrab.grab()
 for i in xrange(Row):
     Row_Read=i+x1
     for j in xrange(Col):
         Col_Read=y1+j
-        clr[Row_Read][Col_Read] = image.getpixel((Row_Read, Col_Read))
-        print clr[Row_Read][Col_Read]
+        color[i][j] = image.getpixel((Row_Read, Col_Read))
+        print color[i][j]
     
